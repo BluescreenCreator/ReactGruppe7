56c9b15ea2d152c410b0bab74b86341a6bd0d23c
Rene <reipoeler.rene@gmail.com>
2021-09-16 17:23:36 +0200

    enstand donnerstag

--- dhbw-todo/src/components/Task.js
+++ dhbw-todo/src/components/Task.js
@@ -4,22 +4,39 @@ import React from "react";
 
 function Task(){
     return(
+        <div>
         <div id = "CreateTask">
             <input id = "TextInput"></input>
             <button onClick = {addToList}>+</button>
+            <button onClick = {ListeLoeschen}>Alles Fertig!</button>
             <div id ="Liste"></div>
+            <button id = "invis" onclick = {loeschen}></button>    
+        </div>
+
+        
+
+
         </div>
     );
+}
+
+function loeschen(event) {
+    event.target.ParentElement.remove();
 }
 
 function addToList(){
     let ListenElement = document.createElement("div");
     let InputWert = document.getElementById("TextInput").value;
-    ListenElement.innerHTML = `<p>${InputWert}</p> <div onClick>Editieren</div> <button onClick = "(this) => {this.ParentElement.remove()}">Entfernen</button>`;
+    ListenElement.innerHTML = `<p>${InputWert}</p> <div onClick>Editieren</div> <button onClick = "function loeschen()">Entfernen</button>`;
     document.getElementById("Liste").appendChild(ListenElement);
 }
 
 
+// diese Funktioen loescht alle Aufgaben der Liste. Dies wird durch ueberschreiben der InnerHTML der gesamten Liste erreicht.
+function ListeLoeschen(){
+    let GesamteListe = document.getElementById("Liste");
+    GesamteListe.innerHTML = ""; 
+}
 
 
 export default Task;
