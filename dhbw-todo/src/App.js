0ba7652fc7164f7648c396f1ddf4fb87d87d4474
NikolayLars <lars.nikolay@online.de>
2021-09-18 19:15:36 +0200

    Edit geht
    Edit geht

--- dhbw-todo/src/App.js
+++ dhbw-todo/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Home from './components/Home';
+import List from './components/List';
 import Task from './components/Task';
 
 function App() {
   return (
     [<Task/>,
-    <Home/>]
+    <Home/>,
+    <List/>]
     
     
   );
--- dhbw-todo/src/components/List.js
+++ dhbw-todo/src/components/List.js
@@ -1,1 +1,32 @@
-// Liste aller Aufgaben
+// Eine Aufgabe
+
+import React from "react";
+
+function List(){
+    return(
+        <div id="getNew">
+       
+            <input id="newtext" type="text"/>
+            <button onClick={changeOld}>Ändern</button>
+        </div>
+    );
+}
+
+function changeOld(){
+    usedField.innerHTML=document.getElementById("newtext").value;
+    document.getElementById("getNew").style.width="0";
+}
+let usedField;
+
+    document.body.addEventListener('click',function() {
+        // eslint-disable-next-line no-restricted-globals
+        let x =event.clientX, y = event.clientY, elm = document.elementFromPoint(x,y);
+            if(elm.tagName==="P"){
+                document.getElementById("getNew").style.width="100px";
+                usedField=elm;
+            }
+        
+        },true)
+
+
+export default List;
